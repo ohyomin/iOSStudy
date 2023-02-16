06cf8f90bcdba29be86ef2894a1babf267ecd28d
HyominOh <ohmnia1112@gmail.com>
2023-02-16 21:31:28 +0900

    Initial Commit

---
+++ MyFirstiOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyFirstiOS
+//
+//  Created by 오효민 on 2023/02/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
